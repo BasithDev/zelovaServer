4833c21a26052ad6693cd6b125ac5009440f330b
abdul basith <BasithDev>
2024-12-19 20:01:40 +0530

    new dist3 file added

---
+++ Client/dist/assets/PrimaryBtn-CsSN_U_4.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-_sbS5Ts6.js";const s=({text:t,onClick:r,className:e=""})=>n.jsx("button",{type:"submit",onClick:r,className:`bg-orange-500 text-white rounded-md hover:bg-orange-600 transition duration-300 ${e}`,children:t});export{s as P};
